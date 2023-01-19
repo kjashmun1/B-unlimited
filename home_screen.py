7b1f7b7dca5c8c21339b7964e11f9387b6c83cce
Preston Watts <pwatts2132@gmail.com>
2023-01-19 16:32:01 -0600

    Changed some home screen format

--- home_screen.py
+++ home_screen.py
@@ -5,13 +5,17 @@ class HomeScreen:
 	def __init__(self, parent):
 		super ().__init__()
 		# create the main frame for the home screen and the required navigation buttons
-		self.main_frame = customtkinter.CTkFrame(master=parent, width=1280, height=720).pack(padx=8, pady=8, fill="both", expand=True)
+		self.main_frame = customtkinter.CTkFrame(master=parent, width=1280, height=720)
+		self.main_frame.pack(padx=8, pady=8, fill="both", expand=True)
 
-		self.inpaint_screen_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Go to Inpaint Screen", border_width=2, command=self.to_inpaint_screen).place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
+		self.inpaint_screen_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Go to Inpaint Screen", border_width=2, command=self.to_inpaint_screen)
+		self.inpaint_screen_button.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-		self.edit_screen_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Go to Edit Screen", border_width=2, command=self.to_edit_screen).place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+		self.edit_screen_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Go to Edit Screen", border_width=2, command=self.to_edit_screen)
+		self.edit_screen_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-		self.exit_program_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Exit Program", border_width=2, command=self.exit_program).place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+		self.exit_program_button = customtkinter.CTkButton(master=self.main_frame, height=100, width=400, text="Exit Program", border_width=2, command=self.exit_program)
+		self.exit_program_button.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
 
 	def to_inpaint_screen(self):
 		print("Going to inpaint screen")
